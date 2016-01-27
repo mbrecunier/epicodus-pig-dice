3d0321c8c0d2af1678c32e94e341022179f9b0c7
Joseph Karasek and Michelle Brecunier <joseph.karasek@gmail.com>
2016-01-27 09:11:10 -0800

    add constructor for Dice, assigns appropriate initial values

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+  function Dice() {
+    this.dieOne = 1;
+    this.dieTwo = 1;
+    this.score = 0;
+  }
---
+++ spec/specs.js
@@ -0,0 +1,9 @@
+describe('Dice', function() {
+  // check the initial values
+  it('creates a Dice object with the proper initial properties', function() {
+    var testDice = new Dice();
+    expect(testDice.dieOne).to.equal(1);
+    expect(testDice.dieTwo).to.equal(1);
+    expect(testDice.score).to.equal(0);
+  });
+});
